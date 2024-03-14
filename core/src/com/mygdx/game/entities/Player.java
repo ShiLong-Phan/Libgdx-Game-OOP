ba94ba792ad689a8c8483e218cf819cfc1547ae5
Phan <phantasmlied@gmail.com>
2024-03-14 20:39:35 +0800

    added vector for button to start scene, can use regular texture as button now.

--- core/src/com/mygdx/game/entities/Player.java
+++ core/src/com/mygdx/game/entities/Player.java
@@ -9,8 +9,6 @@ import com.badlogic.gdx.physics.box2d.World;
 
 public class Player extends Entity{
     private int Currency;
-    private Texture tex;
-    private Pixmap pixmap;
 
     private Body body;
 
@@ -19,9 +17,11 @@ public class Player extends Entity{
         super(world, x,y,width,height, 2, true,cBits, mBits, body);
 
         //resize img
+        Pixmap pixmap;
         Pixmap pixmapOriginal = new Pixmap(Gdx.files.internal("sprites/char.png"));
         pixmap = new Pixmap((int) width, (int) height,pixmapOriginal.getFormat());
         pixmap.drawPixmap(pixmapOriginal,0,0,pixmapOriginal.getWidth(),pixmapOriginal.getHeight(),0,0, pixmap.getWidth(), pixmap.getHeight());
+        Texture tex;
         tex = new Texture(pixmap);
         super.setTex(tex);
         pixmap.dispose();
